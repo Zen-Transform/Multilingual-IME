cf90a38859dbbeaef09bc6f7a9f9341b1119b965
JY <90738099+JY9150@users.noreply.github.com>
2024-08-03 23:15:15 +0800

    add not_implemented

--- multilingual_ime/core/custom_decorators.py
+++ multilingual_ime/core/custom_decorators.py
@@ -7,5 +7,11 @@ def deprecated(func):
         return func(*args, **kwargs)
     return wrapper
 
+def not_implemented(func):
+    def wrapper(*args, **kwargs):
+        warnings.warn(f"\033[91mCall to not implemented function '{func.__name__}'\033[0m", category=DeprecationWarning, stacklevel=2)
+        return func(*args, **kwargs)
+    return wrapper
+
 # Set warnings to raise an error by default
 warnings.simplefilter('error', DeprecationWarning)
