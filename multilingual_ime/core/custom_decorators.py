968d2244ca0415f6dd1a35cb07b9184a0b2dc758
JY <90738099+JY9150@users.noreply.github.com>
2024-08-03 22:50:41 +0800

    add custom decorators

---
+++ multilingual_ime/core/custom_decorators.py
@@ -0,0 +1,11 @@
+
+import warnings
+
+def deprecated(func):
+    def wrapper(*args, **kwargs):
+        warnings.warn(f"\033[91mCall to deprecated function '{func.__name__}'\033[0m", category=DeprecationWarning, stacklevel=2)
+        return func(*args, **kwargs)
+    return wrapper
+
+# Set warnings to raise an error by default
+warnings.simplefilter('error', DeprecationWarning)
