cfe8ebb35a32928cef8e91b7613231ef249e0f1c
JY <90738099+JY9150@users.noreply.github.com>
2024-08-05 23:49:18 +0800

    add test code for separator_converter

--- tests/ime_separator_test.py
+++ tests/ime_separator_test.py
@@ -1,5 +1,6 @@
 import os
 import random
+import re
 from datetime import datetime
 from multiprocessing import Pool
 
@@ -9,6 +10,7 @@ from tqdm import tqdm
 from multilingual_ime.data_preprocess.typo_generater import TypoGenerater
 from multilingual_ime.data_preprocess.keystroke_converter import KeyStrokeConverter
 from multilingual_ime.ime_separator import IMESeparator
+from multilingual_ime.ime_handler import IMEHandler
 from multilingual_ime.core.multi_job_processing import multiprocessing
 
 random.seed(42)
@@ -18,7 +20,7 @@ USER_DEFINE_MAX_DATA_LINE = 20000
 USER_DEFINE_MAX_TEST_LINE = 2000
 CONVERT_LANGUAGES = ["bopomofo", "cangjie", "pinyin", "english"]
 ERROR_TYPE = "random"
-ERROR_RATE = 0
+ERROR_RATE = 0.1
 NUM_OF_MIX_IME = 2
 MIX_WITH_DIFFERENT_NUM_OF_IME = False
 
@@ -47,10 +49,12 @@ SEPARATOR_CONVERTOR_TEST_RESULT_FILE_PATH = (
 def process_line(mix_ime_group: list[(str, str)]) -> str:
     mix_ime_keystrokes, answer_group, plain_texts = "", [], ""
     for ime_type, text in mix_ime_group:
-        keystroke = KeyStrokeConverter.convert(text, convert_type=ime_type)
-        mix_ime_keystrokes += TypoGenerater.generate(
-            keystroke, error_type=ERROR_TYPE, error_rate=ERROR_RATE
+        keystroke = TypoGenerater.generate(
+            KeyStrokeConverter.convert(text, convert_type=ime_type),
+            error_type=ERROR_TYPE,
+            error_rate=ERROR_RATE,
         )
+        mix_ime_keystrokes += keystroke
         answer_group.append(f'("{ime_type}", "{keystroke}")')
         plain_texts += text
 
@@ -74,7 +78,36 @@ def mutiprocess_test(
         "Test_log": f"Input: {mix_ime_keystrokes}\n"
         + f"Label: {separate_answer}\n"
         + f"Output: {separat_result}\n"
-        + f"Output_Len: {len(separat_result)}\n",
+        + f"Output_Len: {len(separat_result)}\n"
+        + f"Correct: {is_correct}\n",
+    }
+
+
+def _separate_characters_and_words(text):
+    # Regular expression to match Chinese characters and English words
+    pattern = re.compile(r"[\u4e00-\u9fff]|[a-zA-Z]+")
+    # Find all matches in the text
+    matches = pattern.findall(text)
+    return matches
+
+
+def batch_sep_conv_test(
+    ime_handler: IMEHandler, mix_ime_keystrokes: str, separate_answer: list
+) -> dict:
+    sentence_candidate_suggestions = ime_handler.get_candidate(mix_ime_keystrokes)
+    is_correct = True
+    if len(separate_answer) != len(sentence_candidate_suggestions):
+        is_correct = False
+    for word_candidates, answer in zip(sentence_candidate_suggestions, separate_answer):
+        if answer not in word_candidates:
+            is_correct = False
+            break
+    return {
+        "Correct": is_correct,
+        "Test_log": f"Input: {mix_ime_keystrokes}\n"
+        + f"Label: {separate_answer}\n"
+        + f"Output: {sentence_candidate_suggestions}\n"
+        + f"Correct: {is_correct}\n",
     }
 
 
@@ -117,62 +150,66 @@ if __name__ == "__main__":
             "Total_lines": MAX_DATA_LINE,
             "NUM_OF_MIX_IME": NUM_OF_MIX_IME,
             "ERROR_RATE": ERROR_RATE,
-            "Mix_count": {
+            "Mix_count": {  # Fix: mix could larger than 2
                 "mix_1": num_of_mix_ime_list.count(1),
                 "mix_2": num_of_mix_ime_list.count(2),
             },
         }
 
-        with tqdm(total=MAX_DATA_LINE) as pbar:
-            with Pool() as pool:
+        with tqdm(total=MAX_DATA_LINE) as pbar, Pool() as pool:
 
-                def updete_pbar(*a):
-                    pbar.update()
+            def updete_pbar(*a):
+                pbar.update()
 
-                reuslt = []
-                for num_of_mix_ime in num_of_mix_ime_list:
-                    sampled_languages = random.sample(
-                        CONVERT_LANGUAGES, k=num_of_mix_ime
+            reuslt = []
+            for num_of_mix_ime in num_of_mix_ime_list:
+                sampled_languages = random.sample(CONVERT_LANGUAGES, k=num_of_mix_ime)
+                mix_ime_group = []
+                for language in sampled_languages:
+                    if language == "english":
+                        mix_ime_group.append((language, english_lines.pop(0)))
+                    else:
+                        mix_ime_group.append((language, chinese_lines.pop(0)))
+
+                reuslt.append(
+                    pool.apply_async(
+                        process_line, args=(mix_ime_group,), callback=updete_pbar
                     )
-                    mix_ime_group = []
-                    for language in sampled_languages:
-                        if language == "english":
-                            mix_ime_group.append((language, english_lines.pop(0)))
-                        else:
-                            mix_ime_group.append((language, chinese_lines.pop(0)))
-
-                    reuslt.append(
-                        pool.apply_async(
-                            process_line, args=(mix_ime_group,), callback=updete_pbar
-                        )
-                    )
-
-                reuslt = [res.get() for res in reuslt]
-                reuslt.insert(0, str(config))
-                with open(TEST_FILE_PATH, "w", encoding="utf-8") as f:
-                    f.write("\n".join(reuslt))
-
-    def test_separator():
-        if os.path.exists(TEST_FILE_PATH):
-            if (
-                input(
-                    f"File {TEST_FILE_PATH} already exists, do you want to overwrite it? (y/n): "
                 )
-                == "y"
-            ):
-                os.remove(TEST_FILE_PATH)
-                generate_mix_ime_test_data()
-        else:
+
+            reuslt = [res.get() for res in reuslt]
+            reuslt.insert(0, str(config))
+            with open(TEST_FILE_PATH, "w", encoding="utf-8") as f:
+                f.write("\n".join(reuslt))
+
+    def _user_want_to_overwrite(file_path: str) -> bool:
+        return (
+            input(
+                f"File {file_path} already exists, do you want to overwrite it? (y/n): "
+            )
+            == "y"
+        )
+
+    def _calculate_len_score(results: dict) -> float:
+        len_score = 0
+        for result in results:
+            numerater = 1 if result["Correct"] else 0
+            denumerator = result["Output_Len"]
+            len_score += numerater / denumerator if denumerator > 0 else 0
+        return len_score
+
+    def test_separator():  # Testing separator on mixed ime data
+        if not os.path.exists(TEST_FILE_PATH) or (
+            os.path.exists(TEST_FILE_PATH) and _user_want_to_overwrite(TEST_FILE_PATH)
+        ):
             generate_mix_ime_test_data()
 
         assert os.path.exists(TEST_FILE_PATH), f"{TEST_FILE_PATH} not found"
 
-        # Testing separator on mixed ime data
         separator = IMESeparator(
             use_cuda=False
         )  # use_cuda=False for multi-processing test
 
-        wrong_answer_logs = []
         with open(TEST_FILE_PATH, "r", encoding="utf-8") as f:
             lines = f.readlines()
             test_config, test_lines = eval(lines[0]), lines[1:]
@@ -186,32 +223,22 @@ if __name__ == "__main__":
                     DATA_AND_LABEL_SPLITTER
                 )[:2]
                 label_answer = eval("[" + line_answer.replace(")(", "), (") + "]")
-                separat_result = separator.separate(mix_ime_keystrokes)
                 results.append(
                     mutiprocess_test(separator, mix_ime_keystrokes, label_answer)
                 )
-
-            total_test_example = len(results)
-            correct_count = 0
-            prediction_len_count = 0
-            prediction_len_count_correct = 0
-            len_score = 0
-            for result in results:
-                if result["Correct"]:
-                    correct_count += 1
-                    prediction_len_count_correct += result["Output_Len"]
-                else:
-                    wrong_answer_logs.append(result["Test_log"])
-
-                numerater = 1 if result["Correct"] else 0
-                denumerator = result["Output_Len"]
-                len_score += numerater / denumerator if denumerator > 0 else 0
-                prediction_len_count += denumerator
 
         except KeyboardInterrupt:
             print("User interrupt")
 
         finally:
+            total_test_example = len(results)
+            correct_count = sum(1 for result in results if result["Correct"])
+            prediction_len_count = sum(result["Output_Len"] for result in results)
+            prediction_len_count_correct = sum(
+                result["Output_Len"] for result in results if result["Correct"]
+            )
+            len_score = _calculate_len_score(results)
+
             print("============= Test Result =============")
             print(f"{test_config}")
             print(
@@ -229,8 +256,68 @@ if __name__ == "__main__":
                     + f"Average Output Len: {prediction_len_count/total_test_example}\n"
                     + f"Average Correct Output Len: {prediction_len_count_correct/total_test_example}\n"
                     + f"Accuracy: {correct_count/total_test_example}, {correct_count}/{total_test_example}\n"
-                    + f"Len Score: {len_score/total_test_example}\n\n"
-                    + f"\n".join(wrong_answer_logs)
+                    + f"Len Score: {len_score/total_test_example}\n"
+                    + f"\n"
+                    + f"\n".join([result["Test_log"] for result in results])
+                )
+
+    def test_separator_conveter():  # Testing separator & converter on mixed ime data
+        ime_handler = IMEHandler()
+        if not os.path.exists(TEST_FILE_PATH) or (
+            os.path.exists(TEST_FILE_PATH) and _user_want_to_overwrite(TEST_FILE_PATH)
+        ):
+            generate_mix_ime_test_data()
+        assert os.path.exists(TEST_FILE_PATH), f"{TEST_FILE_PATH} not found"
+
+        with open(TEST_FILE_PATH, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+            test_config, test_lines = eval(lines[0]), lines[1:]
+            test_lines = random.sample(
+                test_lines, min(len(test_lines), USER_DEFINE_MAX_TEST_LINE)
+            )
+
+        try:
+            results = []
+            for line in tqdm(test_lines):
+                mix_ime_keystrokes, line_plain_text_answer = line.strip().split(
+                    DATA_AND_LABEL_SPLITTER
+                )[0::2]
+
+                results.append(
+                    batch_sep_conv_test(
+                        ime_handler,
+                        mix_ime_keystrokes,
+                        _separate_characters_and_words(line_plain_text_answer),
+                    )
+                )
+
+        except KeyboardInterrupt:
+            print("User interrupt")
+
+        finally:
+            total_test_example = len(results)
+            correct_count = sum(1 for result in results if result["Correct"])
+
+            print("============= Test Result =============")
+            print(f"{test_config}")
+            print(f"Test Date: {TIMESTAMP}")
+            print(f"Total Test Sample: {total_test_example}")
+            print(
+                f"Accuracy: {correct_count/total_test_example}, {correct_count}/{total_test_example}"
+            )
+            with open(
+                SEPARATOR_CONVERTOR_TEST_RESULT_FILE_PATH, "w", encoding="utf-8"
+            ) as f:
+                f.write(
+                    f"============= Test Result =============\n"
+                    + f"{test_config}\n"
+                    + f"Test Date: {TIMESTAMP}\n"
+                    + f"Total Test Sample: {total_test_example}\n"
+                    + f"Correct: {correct_count}\n"
+                    + f"Accuracy: {correct_count/total_test_example}, {correct_count}/{total_test_example}\n"
+                    + f"\n"
+                    + f"\n".join([result["Test_log"] for result in results])
                 )
 
     test_separator()
+    # test_separator_conveter()
