d0d47d481456f3478df006cdb4b718532bdfbc1b
JY <90738099+JY9150@users.noreply.github.com>
2024-10-26 14:02:33 +0800

    test: Add test code for reconstruct_sentence

--- tests/ime_handler_test.py
+++ tests/ime_handler_test.py
@@ -3,14 +3,17 @@ from multilingual_ime.ime_handler import IMEHandler
 ime_handler = IMEHandler()
 
 def test_reconstruct_sentence():
-    test_text = "t g3bjo4dk4apple wathc"
-    label_tokens = ["t ", "g3", "bjo4", "dk4", "apple", " ", "wathc"]
+    test_cases = [
+        ("su3cl3", ["su3", "cl3"]),
+        ("t g3bjo4dk4apple wathc", ["t ", "g3", "bjo4", "dk4", "apple", " ", "wathc"]),
+    ]
+    for test_text, label_tokens in test_cases:
+        tooken_pool = ime_handler._get_token_pool(test_text)
+        possbile_sentences = ime_handler._reconstruct_sentence(test_text, tooken_pool)
+        for sentence in possbile_sentences:
+            assert "".join(sentence) == test_text, "Reconstructed sentence is not same as input text"
 
-    possbile_sentences = ime_handler._reconstruct_sentence(test_text)
-    for sentence in possbile_sentences:
-        assert "".join(sentence) == test_text, "Reconstructed sentence is not same as input text"
-
-    assert label_tokens in possbile_sentences, "Label tokens not in possible sentence"
+        assert label_tokens in possbile_sentences, "Label tokens not in possible sentence"
 
 
 def test_get_token_pool():
