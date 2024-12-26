521baacb503ca28b4d914966fe31f47ed566cc4b
JY <90738099+JY9150@users.noreply.github.com>
2024-12-26 20:00:51 +0800

    fix: Fix "recursive use of cursors" by adding thread lock

--- multilingual_ime/keystroke_map_db.py
+++ multilingual_ime/keystroke_map_db.py
@@ -1,10 +1,10 @@
-from pathlib import Path
 import sqlite3
+import threading
+from pathlib import Path
 
 from .trie import modified_levenshtein_distance
 from .core.custom_decorators import lru_cache_with_doc
 
-# from fuzzywuzzy import fuzz
 
 MAX_LEVENSHTEIN_DISTANCE = 1
 
@@ -16,24 +16,27 @@ class KeystrokeMappingDB:
 
         self._conn = sqlite3.connect(db_path, check_same_thread=False)
         self._cursor = self._conn.cursor()
+        self._lock = threading.Lock()
         self._conn.create_function("levenshtein", 2, modified_levenshtein_distance)
 
     def get(self, keystroke: str) -> list[str]:
-        self._cursor.execute(
-            "SELECT keystroke, word, frequency FROM keystroke_map WHERE keystroke = ?",
-            (keystroke,),
-        )
-        return self._cursor.fetchall()
+        with self._lock:
+            self._cursor.execute(
+                "SELECT keystroke, word, frequency FROM keystroke_map WHERE keystroke = ?",
+                (keystroke,),
+            )
+            return self._cursor.fetchall()
 
     @lru_cache_with_doc(maxsize=128)
     def fuzzy_get(
         self, keystroke: str, max_distance: int = MAX_LEVENSHTEIN_DISTANCE
     ) -> list[str]:
-        self._cursor.execute(
-            f"SELECT keystroke, word, frequency FROM keystroke_map WHERE levenshtein(keystroke, ?) <= {max_distance}",
-            (keystroke,),
-        )
-        return self._cursor.fetchall()
+        with self._lock:
+            self._cursor.execute(
+                f"SELECT keystroke, word, frequency FROM keystroke_map WHERE levenshtein(keystroke, ?) <= {max_distance}",
+                (keystroke,),
+            )
+            return self._cursor.fetchall()
 
     @lru_cache_with_doc(maxsize=128)
     def get_closest(self, keystroke: str) -> list[tuple[str, str, int]]:
@@ -58,29 +61,31 @@ class KeystrokeMappingDB:
                 return result
         return []
 
-    def create_table(self):
-        self._cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS keystroke_map (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                keystroke TEXT,
-                word TEXT,
-                frequency INTEGER
-            )
-            """
-        )
-        self._cursor.execute(
-            "CREATE INDEX IF NOT EXISTS idx_keystroke ON keystroke_map (keystroke)"
-        )
-        self._conn.commit()
-
-    def insert(self, keystroke: str, word: str, frequency: int):
-        if (keystroke, word, frequency) not in self.get(keystroke):
+    def create_keystroke_map_table(self):
+        with self._lock:
             self._cursor.execute(
-                "INSERT INTO keystroke_map (keystroke, word, frequency) VALUES (?, ?, ?)",
-                (keystroke, word, frequency),
+                """
+                CREATE TABLE IF NOT EXISTS keystroke_map (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    keystroke TEXT,
+                    word TEXT,
+                    frequency INTEGER
+                )
+                """
+            )
+            self._cursor.execute(
+                "CREATE INDEX IF NOT EXISTS idx_keystroke ON keystroke_map (keystroke)"
             )
             self._conn.commit()
+
+    def insert(self, keystroke: str, word: str, frequency: int):
+        with self._lock:
+            if (keystroke, word, frequency) not in self.get(keystroke):
+                self._cursor.execute(
+                    "INSERT INTO keystroke_map (keystroke, word, frequency) VALUES (?, ?, ?)",
+                    (keystroke, word, frequency),
+                )
+                self._conn.commit()
 
     def insert_many(self, data: list[tuple[str, str, int]]):
         for keystroke, word, frequency in data:
@@ -106,43 +111,48 @@ class KeystrokeMappingDB:
                 distance += 1
 
     def word_to_keystroke(self, word: str) -> str:
-        self._cursor.execute(
-            "SELECT keystroke FROM keystroke_map WHERE word = ?", (word,)
-        )
-        if word := self._cursor.fetchone():
-            return word[0]
-        else:
-            return None
+        with self._lock:
+            self._cursor.execute(
+                "SELECT keystroke FROM keystroke_map WHERE word = ?", (word,)
+            )
+            if word := self._cursor.fetchone():
+                return word[0]
+            else:
+                return None
 
     def word_exists(self, word: str) -> bool:
-        self._cursor.execute("SELECT word FROM keystroke_map WHERE word = ?", (word,))
-        return bool(self._cursor.fetchone())
+        with self._lock:
+            self._cursor.execute(
+                "SELECT word FROM keystroke_map WHERE word = ?", (word,)
+            )
+            return bool(self._cursor.fetchone())
 
     def increment_word_frequency(self, word: str):
-        self._cursor.execute(
-            "UPDATE keystroke_map SET frequency = frequency + 1 WHERE word = ?",
-            (word,),
-        )
-        self._conn.commit()
+        with self._lock:
+            self._cursor.execute(
+                "UPDATE keystroke_map SET frequency = frequency + 1 WHERE word = ?",
+                (word,),
+            )
+            self._conn.commit()
 
     def get_word_frequency(self, word: str) -> int:
-        self._cursor.execute(
-            "SELECT frequency FROM keystroke_map WHERE word = ?", (word,)
-        )
-        if frequency := self._cursor.fetchone():
-            return frequency[0]
-        else:
-            return 0
+        with self._lock:
+            self._cursor.execute(
+                "SELECT frequency FROM keystroke_map WHERE word = ?", (word,)
+            )
+            if frequency := self._cursor.fetchone():
+                return frequency[0]
+            else:
+                return 0
 
 
 if __name__ == "__main__":
     import pathlib
     import time
+
     db_path = pathlib.Path(__file__).parent / "src" / "bopomofo_keystroke_map.db"
 
-    db = KeystrokeMappingDB(
-        db_path
-    )
+    db = KeystrokeMappingDB(db_path)
     start_time = time.time()
     test_cases = ["su3", "cl3", "t ", "gjo4", "s86", "1p4", "ru04", "1l4", "fu04", "503", "g8 ", "al6", "xu4", "b.6", "wj/ ", "1u/4", "fu06", "1l4", "ejo3", "w.6"]
     for test_case in test_cases:
--- multilingual_ime/phrase_db.py
+++ multilingual_ime/phrase_db.py
@@ -1,3 +1,4 @@
+import threading
 from pathlib import Path
 
 import sqlite3
@@ -10,63 +11,71 @@ class PhraseDataBase:
 
         self._conn = sqlite3.connect(db_path, check_same_thread=False)
         self._cursor = self._conn.cursor()
+        self._lock = threading.Lock()
 
     def __del__(self) -> None:
         self._conn.commit()
         self._conn.close()
 
-    def create_table(self) -> None:
-        self._cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS phrase_table (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                phrase TEXT,
-                frequency INTEGER
+    def create_phrase_table_table(self) -> None:
+        with self._lock:
+            self._cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS phrase_table (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    phrase TEXT,
+                    frequency INTEGER
+                )
+                """
             )
-            """
-        )
-        self._conn.commit()
+            self._conn.commit()
 
     def getphrase(self, phrase: str) -> list[tuple[str, int]]:
-        self._cursor.execute(
-            f"SELECT phrase FROM phrase_table WHERE phrase = '{phrase}'"
-        )
-        return [row[0] for row in self._cursor.fetchall()]
+        with self._lock:
+            self._cursor.execute(
+                f"SELECT phrase FROM phrase_table WHERE phrase = '{phrase}'"
+            )
+            return [row[0] for row in self._cursor.fetchall()]
 
     def get_phrase_with_prefix(self, prefix: str) -> list[tuple[str, int]]:
         if not prefix:
             return []
-
-        self._cursor.execute(
-            f"SELECT phrase, frequency FROM phrase_table WHERE phrase LIKE '{prefix}%'"
-        )
-        return self._cursor.fetchall()
+        with self._lock:
+            self._cursor.execute(
+                f"SELECT phrase, frequency FROM phrase_table WHERE phrase LIKE '{prefix}%'"
+            )
+            return self._cursor.fetchall()
 
     def insert(self, phrase: str, frequency: int) -> None:
         if not self.getphrase(phrase):
-            self._cursor.execute(
-                f"INSERT INTO phrase_table (phrase, frequency) VALUES ('{phrase}', {frequency})"
-            )
-            self._conn.commit()
+            with self._lock:
+                self._cursor.execute(
+                    f"INSERT INTO phrase_table (phrase, frequency) VALUES ('{phrase}', {frequency})"
+                )
+                self._conn.commit()
     
     def update(self, phrase: str, frequency: int) -> None:
         if not self.getphrase(phrase):
             self.insert(phrase, frequency)
-        self._cursor.execute(
-            f"UPDATE phrase_table SET frequency = {frequency} WHERE phrase = '{phrase}'"
-        )
-        self._conn.commit()
+
+        with self._lock:
+            self._cursor.execute(
+                f"UPDATE phrase_table SET frequency = {frequency} WHERE phrase = '{phrase}'"
+            )
+            self._conn.commit()
 
     def delete(self, phrase: str) -> None:
-        self._cursor.execute(f"DELETE FROM phrase_table WHERE phrase = '{phrase}'")
-        self._conn.commit()
+        with self._lock:
+            self._cursor.execute(f"DELETE FROM phrase_table WHERE phrase = '{phrase}'")
+            self._conn.commit()
 
     def increment_frequency(self, phrase: str) -> None:
-        self._cursor.execute(
-            f"UPDATE phrase_table SET frequency = frequency + 1 WHERE phrase = '{phrase}'"
-        )
-        self._conn.commit()
+        with self._lock:
+            self._cursor.execute(
+                f"UPDATE phrase_table SET frequency = frequency + 1 WHERE phrase = '{phrase}'"
+            )
+            self._conn.commit()
 
 if __name__ == "__main__":
     db = PhraseDataBase(Path(__file__).parent / "src" / "chinese_phrase.db")
-    db.create_table()
+    db.create_phrase_table_table()
