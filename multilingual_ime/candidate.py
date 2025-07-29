6da147436b6a3afc9bf41991713b68704f2efccd
JY <90738099+JY9150@users.noreply.github.com>
2025-07-29 21:16:04 +0800

    refactor: Add type hints and docstring

--- multilingual_ime/candidate.py
+++ multilingual_ime/candidate.py
@@ -1,33 +1,27 @@
 from colorama import Fore, Style
 
-from multilingual_ime.core.custom_decorators import deprecated
-
-@deprecated("This class is deprecated, use 'Candidate' instead.")
-class CandidateWord:
-    def __init__(self, word, keystrokes, word_frequency):
-        self.word = word
-        self.keystrokes = keystrokes
-        self.word_frequency = word_frequency
-        self.user_key = None
-        self.distance = None
-        self.method = None
-
-    def to_dict(self):
-        return {
-            "word": self.word,
-            "keystrokes": self.keystrokes,
-            "word_frequency": self.word_frequency,
-            "user_key": self.user_key,
-            "distance": self.distance,
-        }
-
-    def set_method(self, method: str):
-        self.method = method
-        return self
-
 
 class Candidate:
-    def __init__(self, word, keystrokes, word_frequency, user_key, distance, method):
+    """
+    Represents a candidate word in an input method editor (IME) system.
+    Attributes:
+        word (str): The candidate word.
+        keystrokes (str): The keystrokes associated with the candidate word.
+        word_frequency (int | str): The frequency of the word, can be an integer or a string.
+        user_key (str): The user key associated with the candidate.
+        distance (int | str): The distance metric for the candidate, can be an integer or a string.
+        ime_method (str): The input method used for this candidate.
+    """
+
+    def __init__(
+        self,
+        word: str,
+        keystrokes: str,
+        word_frequency: int,
+        user_key: str,
+        distance: int,
+        method: str,
+    ):
         self.__word = word
         self.__keystrokes = keystrokes
         self.__word_frequency = word_frequency
@@ -37,29 +31,55 @@ class Candidate:
 
     @property
     def word(self):
+        """
+        Returns the candidate word.
+        """
         return self.__word
 
     @property
     def keystrokes(self):
+        """
+        Returns the keystrokes associated with the candidate word.
+        """
         return self.__keystrokes
 
     @property
     def word_frequency(self):
+        """
+        Returns the word frequency of the candidate word.
+        """
         return self.__word_frequency
 
     @property
     def user_key(self):
+        """
+        Returns the original user key associated with the candidate word.
+        """
         return self.__user_key
 
     @property
     def distance(self):
+        """
+        Returns the distance of the candidate word and the user key.
+        This can be used to determine how closely the candidate matches the user's input.
+        """
         return self.__distance
 
     @property
     def ime_method(self):
+        """
+        Returns the input method of the candidate word.
+        This indicates which input method was used to generate the candidate.
+        """
         return self.__ime_method
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
+        """
+        Converts the candidate object to a dictionary representation.
+
+        Returns:
+            dict: A dictionary representation of the candidate object.
+        """
         return {
             "word": self.word,
             "keystrokes": self.keystrokes,
@@ -93,5 +113,3 @@ if __name__ == "__main__":
     cand1 = Candidate("word", "keystrokes", 1, "user_key", 1, "method")
     print(cand.to_dict())
     print(cand1.to_dict())
-    cand1.word = "new_word1"
-    cand.word = "new_word"
